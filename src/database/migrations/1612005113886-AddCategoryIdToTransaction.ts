a367ddfeeafa8d8a797af9fb3b059ca8bfa9e3b5
Deive Santana <deliriousnow7@gmail.com>
2021-01-30 11:27:44 -0300

    finished challenge

---
+++ src/database/migrations/1612005113886-AddCategoryIdToTransaction.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCategoryIdToTransaction1612005113886
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'CategoryID',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'CategoryID');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,70 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (value <= 0) {
+      throw new AppError('Cannot operate negative numbers');
+    }
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+      if (value > balance.total) {
+        throw new AppError('Total balance insuficient');
+      }
+    }
+    const titleExists = await transactionsRepository.findOne({
+      where: { title },
+    });
+    if (titleExists) {
+      throw new AppError('Cannot create transaction with same title');
+    }
+    const categoryRepository = getRepository(Category);
+
+    const categoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      const newCategory = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(newCategory);
+    }
+
+    const category_data = await categoryRepository.findOne({
+      where: { title: category },
+    });
+    const category_id = category_data?.id;
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
